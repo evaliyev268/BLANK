97eecc257026bcbb5a32e6caa20098d1262469ae
Elvin Valiyev <evaliyev268@gmail.com>
2025-12-19 12:00:00 +0300

    Vy

---
+++ ForRMQ/RMQ.Consumer/Program.cs
@@ -0,0 +1,26 @@
+﻿// Consumer
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory=new();
+
+factory.Uri = new("amqps://rpjqjzap:SATQLbg_IDrbdTOZL8spYb8YwPkeO888@rabbit.lmq.cloudamqp.com/rpjqjzap");
+
+using IConnection conn=factory.CreateConnection();
+using IModel ch=conn.CreateModel();
+
+ch.QueueDeclare(queue:"example-queue", exclusive:false);
+
+EventingBasicConsumer consumer = new(ch);
+
+consumer.Received += (sender,e) => {
+
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+
+};
+
+ch.BasicConsume(queue:"example-queue", autoAck:false, consumer);
+
+Console.Read();
---
+++ ForRMQ/RMQ.Publisher/Program.cs
@@ -0,0 +1,22 @@
+﻿//Publisher
+
+
+
+using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+
+factory.Uri = new("amqps://rpjqjzap:SATQLbg_IDrbdTOZL8spYb8YwPkeO888@rabbit.lmq.cloudamqp.com/rpjqjzap");
+
+using IConnection conn = factory.CreateConnection();
+using IModel ch = conn.CreateModel();
+
+
+ch.QueueDeclare(queue: "example-queue", exclusive: false);
+
+byte[] message = Encoding.UTF8.GetBytes("Elvinus Valiyevus");
+
+ch.BasicPublish(exchange: "", routingKey: "job-example", body: message);
+
+Console.Read();
